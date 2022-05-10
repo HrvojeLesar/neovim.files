8c948c7f6075ea0e959b31adfdadf78615c4ebc7
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2022-05-10 18:29:07 +0200

    Updated nvim-lsp-installer config.

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -79,26 +79,21 @@ cmp.setup({
 })
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
 
-local lsp_installer = require("nvim-lsp-installer")
+local lsp_installer = require('nvim-lsp-installer');
+lsp_installer.setup();
 
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
+local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+for _, server in ipairs(lsp_installer.get_installed_servers()) do
+    require('lspconfig')[server.name].setup(opts);
+end
+    
 
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
 
 require'rust-tools'.setup({
     tools = {
