0dd05d150cce503302beb1ac5e6d5b71fda277e2
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2022-07-25 13:36:28 +0200

    2 lines written better.

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -93,8 +93,8 @@ cmp.setup({
     })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local lsp_installer = require('nvim-lsp-installer');
 lsp_installer.setup();
