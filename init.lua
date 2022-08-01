8afcd26bcb5f7a57ec11acc16d648036b47e881b
Hrvoje Lesar <hrvek@vmi423282.contaboserver.net>
2022-08-01 13:48:56 +0200

    Fixed setting wrong undodir on linux.

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ if vim.fn.has("win32") == 1 then
     vim.keymap.set("n", "<C-z>", "<Nop>", {})
     vim.opt.undodir = os.getenv("UserProfile") .. "/.nvim/undodir"
 else
-    vim.opt.undodir = "~/.nvim/undodir"
+    vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
 end
 
 -- stop vscode extension to try and start neovim plugins
