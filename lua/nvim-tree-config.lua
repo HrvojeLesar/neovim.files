dfe76d8f6ae953299a52731c94202ce95ec26e89
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-02-12 12:25:00 +0100

    Nvim tree opens as expected when using directories, also cds to current directory directly opening a file.

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -12,3 +12,23 @@ require("nvim-tree").setup({
 
 vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", {})
 vim.api.nvim_set_keymap("n", "<Leader>n", ":NvimTreeFindFile<CR>", {})
+
+local function open_nvim_tree(data)
+
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if directory then
+        -- change to the directory
+        vim.cmd.cd(data.file)
+        -- open the tree
+        require("nvim-tree.api").tree.open()
+    else
+        vim.cmd.cd("%:h")
+    end
+
+
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
