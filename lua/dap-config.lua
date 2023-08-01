643c0c15c20e9ea45fc509bc2c6f8d2666fd6168
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-08-01 22:08:34 +0200

    More dap config

--- lua/dap-config.lua
+++ lua/dap-config.lua
@@ -1,22 +1,24 @@
-local buf_set_keymap = vim.api.nvim_set_keymap
-opts = { noremap = true, silent = true }
-
-buf_set_keymap("n", "<F5>", '<cmd>lua require("dap").continue()<CR>', opts)
-buf_set_keymap("n", "<F10>", '<cmd>lua require("dap").step_over()<CR>', opts)
-buf_set_keymap("n", "<F11>", '<cmd>lua require("dap").step_into()<CR>', opts)
-buf_set_keymap("n", "<F12>", '<cmd>lua require("dap").step_out()<CR>', opts)
-buf_set_keymap("n", "<leader>b", '<cmd>lua require("dap").toggle_breakpoint()<CR>', opts)
-buf_set_keymap("n", "<leader>B", '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
-    opts)
-buf_set_keymap("n", "<leader>lp",
-    '<cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', opts)
-buf_set_keymap("n", "<leader>dr", '<cmd>lua require("dap").repl.open()<CR>', opts)
-buf_set_keymap("n", "<leader>dl", '<cmd>lua require("dap").repl.run_last()<CR>', opts)
-buf_set_keymap("n", "<leader>dd", '<cmd>lua local widgets=require"dap.ui.widgets"; widgets.centered_float(widgets.scopes)<CR>', opts)
-
-buf_set_keymap("n", "<leader>dd", '<cmd>lua require("dapui").toggle()<CR>', opts)
-
 local dap = require("dap")
+local dapui = require("dapui")
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<F5>", dap.continue, opts)
+vim.keymap.set("n", "<F10>", dap.step_over, opts)
+vim.keymap.set("n", "<F11>", dap.step_into, opts)
+vim.keymap.set("n", "<F12>", dap.step_out, opts)
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, opts)
+vim.keymap.set("n", "<leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, opts)
+vim.keymap.set("n", "<leader>lp", function() dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, opts)
+vim.keymap.set("n", "<leader>dr", dap.repl.open, opts)
+vim.keymap.set("n", "<leader>dl", dap.run_last, opts)
+
+-- vim.keymap.set("n", "<leader>dd", local widgets=require"dap.ui.widgets"; widgets.centered_float(widgets.scopes), opts)
+vim.keymap.set("n", "<leader>dd", dapui.toggle, opts)
+
+vim.keymap.set({"n", "v"}, "<leader>K", dapui.eval, opts)
+vim.keymap.set({"n", "v"}, "<leader>k", function() dapui.float_element("scopes", { enter = true }) end, opts)
+
 local registry = require("mason-registry")
 
 dap.adapters.delve = {
@@ -76,63 +78,6 @@ dap.configurations.php = {
         port = 9003
     }
 }
-
-dap.set_log_level("DEBUG")
-
-dap.adapters.cppdbg = {
-    type = "executable",
-    command = vim.fn.stdpath("data") .. "\\cpptools\\debugAdapters\\bin\\OpenDebugAD7.exe",
-    args = { "--server" },
-}
-
-dap.adapters.codelldb = function(on_adapter)
-    local stdout = vim.loop.new_pipe(false)
-    local stderr = vim.loop.new_pipe(false)
-
-    local cmd = vim.fn.stdpath("data") .. "\\codelldb\\adapter\\codelldb.exe"
-
-    local handle, pid_or_err
-    local opts = {
-        stdio = { nil, stdout, stderr },
-        detached = true,
-    }
-    handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
-        stdout:close()
-        stderr:close()
-        handle:close()
-        if code ~= 0 then
-            print("codelldb exited with code", code)
-        end
-    end)
-    assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
-    stdout:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-            local port = chunk:match("Listening on port (%d+)")
-            if port then
-                vim.schedule(function()
-                    on_adapter({
-                        type = "server",
-                        host = "127.0.0.1",
-                        port = port
-                    })
-                end)
-            else
-                vim.schedule(function()
-                    require("dap.repl").append(chunk)
-                end)
-            end
-        end
-    end)
-    stderr:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-            vim.schedule(function()
-                require("dap.repl").append(chunk)
-            end)
-        end
-    end)
-end
 
 dap.configurations.cpp = {
     {
