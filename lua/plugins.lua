08365d02f074ed581f6636f1d80db98bd48d5a09
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-10-06 12:37:16 +0200

    Indent character colour.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -201,8 +201,16 @@ return require("packer").startup(function(use)
     use {
         "lukas-reineke/indent-blankline.nvim",
         config = function()
+            local hooks = require "ibl.hooks"
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "CustomBlanklineIndentChar", { fg = "#31353f" })
+            end)
+
             require("ibl").setup({
-                indent = { char = "▏" },
+                indent = { 
+                    char = "│",
+                    highlight = "CustomBlanklineIndentChar"
+                },
                 scope = { enabled = false },
             })
         end
