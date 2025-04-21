24b14e177bf7fb1052d7a182f31b4394b7ba76da
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-04-21 13:52:31 +0200

    markdown-preview changed package manager from yarn to npm

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,11 @@
 return {
     {
         "iamcco/markdown-preview.nvim",
-        build = "cd app && yarn install",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
     },
 }
