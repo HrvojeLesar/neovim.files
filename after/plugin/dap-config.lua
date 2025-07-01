de0b67c5a44c64f0390349ff36dc83e41f9ceacb
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-07-01 20:09:27 +0200

    lldb dap

--- after/plugin/dap-config.lua
+++ after/plugin/dap-config.lua
@@ -105,3 +105,12 @@ dap.configurations.php = {
 dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
 end
+
+dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+        command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+        args = { "--port", "${port}" },
+    },
+}
