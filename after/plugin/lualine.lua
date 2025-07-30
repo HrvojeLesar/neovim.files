a8798c551eab298f10d61a59dda7befb35e383a5
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-07-30 11:42:44 +0200

    reenabled codeium

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,19 @@
 local function codeium_status()
-    return vim.api.nvim_call_function("codeium#GetStatusString", {})
+    local status = require('codeium.virtual_text').status()
+
+    if status.state == 'idle' then
+        return ''
+    end
+
+    if status.state == 'waiting' then
+        return "..."
+    end
+
+    if status.state == 'completions' and status.total > 0 then
+        return string.format('%d/%d', status.current, status.total)
+    end
+
+    return ''
 end
 
 require("lualine").setup({
