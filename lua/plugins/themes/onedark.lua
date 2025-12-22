2c69ac3c41b320e05475cf770e8c82b579b151ab
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-12-22 16:42:29 +0100

    Updated onedark theme startup

--- lua/plugins/themes/onedark.lua
+++ lua/plugins/themes/onedark.lua
@@ -1,3 +1,29 @@
 return {
-	{ "navarasu/onedark.nvim" },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config = function()
+            local onedark = require("onedark")
+            local palette = require("onedark.palette")
+            local transparent = function()
+                if vim.fn.has("win32") == 1 then
+                    return false
+                else
+                    return true
+                end
+            end
+            onedark.setup({
+                diagnostics = {
+                    undercurl = false,
+                },
+                transparent = transparent(),
+                highlights = {
+                    LspReferenceText = { bg = palette.dark.bg1 },
+                    LspReferenceWrite = { bg = palette.dark.bg1 },
+                    LspReferenceRead = { bg = palette.dark.bg1 },
+                },
+            })
+            onedark.load()
+        end,
+    },
 }
