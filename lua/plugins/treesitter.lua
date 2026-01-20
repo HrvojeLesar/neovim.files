834823a4d6d19088ccfedb7bb0621e9433277444
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-01-20 10:07:52 +0100

    Updated treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,34 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		version = "0.10.0",
+		branch = "master",
+		lazy = false,
 		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"rust",
+					"javascript",
+					"typescript",
+					"go",
+					"html",
+					"css",
+					"php",
+					"tsx",
+					"vimdoc",
+					"json",
+				},
+				indent = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
 	},
 	{ "nvim-treesitter/nvim-treesitter-context" },
 }
